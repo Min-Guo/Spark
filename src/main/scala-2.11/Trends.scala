4e16817c8e00a4fe9d6992a82d2b21e8dd0aac30
Min Guo <xmguomin@gmail.com>
2016-07-14 16:56:07 -0700

    Get tweets

--- src/main/scala-2.11/Trends.scala
+++ src/main/scala-2.11/Trends.scala
@@ -1,4 +1,5 @@
-import twitter4j.{Trends, TwitterFactory, Twitter}
+
+import twitter4j.{QueryResult, Trends, TwitterFactory, Twitter, Query, Status, TwitterException}
 import twitter4j.conf.ConfigurationBuilder
 
 import scala.collection.mutable.ArrayBuffer
@@ -6,7 +7,7 @@ import scala.collection.mutable.ArrayBuffer
 object TrendingTopics {
   def getTrends(f: Unit): ArrayBuffer[String] = {
 
-    // (1) config work to create a twitter object
+    //config work to create a twitter object
     val cb = new ConfigurationBuilder()
     cb.setDebugEnabled(true)
       .setOAuthConsumerKey("dvUkoBr8N3kePgtaNXgFqIW2E")
@@ -16,7 +17,7 @@ object TrendingTopics {
     val tf = new TwitterFactory(cb.build())
     val twitter = tf.getInstance()
 
-    // (2) use the twitter object to get your friend's timeline
+    //use the twitter object to get your friend's timeline
     val trends = twitter.getPlaceTrends(23424977);
     var trendingWords: ArrayBuffer[String] = new ArrayBuffer[String]()
     for (i <- trends.getTrends()) {
@@ -25,6 +26,7 @@ object TrendingTopics {
     return trendingWords;
   }
 
+  //remove hashTag
   def parseTopics(f: ArrayBuffer[String]): ArrayBuffer[String] = {
     val noTagWords: ArrayBuffer[String] = new ArrayBuffer[String]();
     for (i <- f) {
@@ -33,10 +35,41 @@ object TrendingTopics {
     return noTagWords
   }
 
-  def main(args: Array[String]): Unit = {
-    val words: ArrayBuffer[String] = parseTopics(getTrends());
-    for (i <- words) {
-      System.out.println(i);
+  //search query
+  def searchQuery(f: ArrayBuffer[String]) = {
+    for (i <- f) {
+      //config work to create a twitter object
+      val cb = new ConfigurationBuilder()
+      cb.setDebugEnabled(true)
+        .setOAuthConsumerKey("dvUkoBr8N3kePgtaNXgFqIW2E")
+        .setOAuthConsumerSecret("6Yix2c6gn5oGbOcdDsIgLkLy4EoJvjdArl2wcVnJT2hkdJBeA0")
+        .setOAuthAccessToken("2523499370-jKz9tm4RWh96HcNs1G6kN5wMsUeuT3eJXSGoiAV")
+        .setOAuthAccessTokenSecret("Wy29SE0LZBL2xoHo3mAv17e4mSNYK18Hfh59dzDSUzW9i")
+      val tf = new TwitterFactory(cb.build())
+      val twitter = tf.getInstance()
+      val query = new Query(i)
+      query.setCount(10)
+      try {
+        val queryResult = twitter.search(query)
+        val tweets = queryResult.getTweets()
+        val it = tweets.iterator();
+        while (it.hasNext()) {
+          val status = it.next()
+          //val country = status.getPlace()
+          //val location = status.getPlace().getCountry().equals("Germany")
+          System.out.println(i)
+          System.out.println(status.getText())
+
+        }
+      }
+      catch {
+        case te: TwitterException => println("exception caught: " + te);
+      }
     }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val words: ArrayBuffer[String] = parseTopics(getTrends())
+    searchQuery(words)
   }
 }
